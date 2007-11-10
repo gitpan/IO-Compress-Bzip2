675f37309883b6ee697fbf3005f76f3b57e3131d
Paul Marquess <pmqs@cpan.org>
2007-11-10 11:59:59 +0000

    Import of PMQS/IO-Compress-Bzip2-2.008 from CPAN.
    gitpan-cpan-distribution: IO-Compress-Bzip2
    gitpan-cpan-version:      2.008
    gitpan-cpan-path:         PMQS/IO-Compress-Bzip2-2.008.tar.gz
    gitpan-cpan-author:       PMQS
    gitpan-cpan-maturity:     released

--- lib/IO/Compress/Adapter/Bzip2.pm
+++ lib/IO/Compress/Adapter/Bzip2.pm
@@ -4,13 +4,13 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.006 qw(:Status);
+use IO::Compress::Base::Common  2.008 qw(:Status);
 
 #use Compress::Bzip2 ;
-use Compress::Raw::Bzip2  2.006 ;
+use Compress::Raw::Bzip2  2.008 ;
 
 our ($VERSION);
-$VERSION = '2.006';
+$VERSION = '2.008';
 
 sub mkCompObject
 {
