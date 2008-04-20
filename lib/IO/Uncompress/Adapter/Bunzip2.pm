8aa20c0b8d5f85c81898cd9adf13132e6a3eff89
Paul Marquess <pmqs@cpan.org>
2008-04-20 14:44:25 +0000

    Import of PMQS/IO-Compress-Bzip2-2.009 from CPAN.
    gitpan-cpan-distribution: IO-Compress-Bzip2
    gitpan-cpan-version:      2.009
    gitpan-cpan-path:         PMQS/IO-Compress-Bzip2-2.009.tar.gz
    gitpan-cpan-author:       PMQS
    gitpan-cpan-maturity:     released

--- lib/IO/Uncompress/Adapter/Bunzip2.pm
+++ lib/IO/Uncompress/Adapter/Bunzip2.pm
@@ -4,13 +4,13 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common 2.008 qw(:Status);
+use IO::Compress::Base::Common 2.009 qw(:Status);
 
 #use Compress::Bzip2 ;
-use Compress::Raw::Bzip2 2.008 ;
+use Compress::Raw::Bzip2 2.009 ;
 
 our ($VERSION, @ISA);
-$VERSION = '2.008';
+$VERSION = '2.009';
 
 #@ISA = qw( Compress::Raw::Bunzip2 );
 
